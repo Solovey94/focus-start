2232ba4f82d5924ddeff25d8eb6ec3ad49206e10
Alexey Solovey <solovei_94@mail.ru>
2019-11-19 00:13:16 +0400

    implemented AddressService methods

--- src/main/java/mocks/crud/task/service/AddressService.java
+++ src/main/java/mocks/crud/task/service/AddressService.java
@@ -3,34 +3,43 @@ package mocks.crud.task.service;
 import mocks.crud.task.model.Address;
 import mocks.crud.task.repository.CrudRepository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AddressService implements CrudRepository<Long, Address> {
+
+    private Map<Long,Address> address = new HashMap<>();
+
     @Override
     public void save(Address element) {
         //todo написать реализацию
+        address.put(element.getId(), element);
     }
 
     @Override
     public Address findById(Long id) {
         //todo написать реализацию
-        return null;
+        return address.getOrDefault(id, null);
     }
 
     @Override
     public List<Address> findAll() {
         //todo написать реализацию
-        return null;
+        return new ArrayList<>(address.values());
     }
 
     @Override
     public Address update(Address element) {
         //todo написать реализацию
-        return null;
+        address.put(element.getId(),element);
+        return element;
     }
 
     @Override
     public void delete(Address element) {
         //todo написать реализацию
+        address.remove(element.getId());
     }
 }
