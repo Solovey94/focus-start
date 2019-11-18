7026da0846257ca6957eedc235ba616132880a89
Alexey Solovey <solovei_94@mail.ru>
2019-11-19 00:38:19 +0400

    implemented PersonService methods

--- src/main/java/mocks/crud/task/service/PersonService.java
+++ src/main/java/mocks/crud/task/service/PersonService.java
@@ -5,13 +5,15 @@ import mocks.crud.task.model.Person;
 import mocks.crud.task.repository.AdvancedRepository;
 import mocks.crud.task.repository.CrudRepository;
 
-import java.util.List;
+import java.util.*;
 
-public class PersonService implements  AdvancedRepository {
+public class PersonService implements AdvancedRepository {
 
     private AddressService addressService;
 
     private CrudRepository<Long, Person> personRepository;
+
+    private Map<Long, Person> persons = new HashMap<>();
 
     public PersonService(AddressService addressService) {
         this.addressService = addressService;
@@ -20,35 +22,38 @@ public class PersonService implements  AdvancedRepository {
     @Override
     public List<Person> findAllRelatives(Person person) {
         //todo написать реализацию
-        return null;
+        return person.getRelatives();
     }
 
     @Override
     public Address getAddress(Person person) {
         //todo написать реализацию
-        return null;
+        return person.getAddress();
     }
 
     public void save(Person element) {
         //todo написать реализацию
+        persons.put(element.getId(), element);
     }
 
     public Person findById(Long id) {
         //todo написать реализацию
-        return null;
+        return persons.getOrDefault(id, null);
     }
 
     public List<Person> findAll() {
         //todo написать реализацию
-        return null;
+        return new ArrayList<>(persons.values());
     }
 
     public Person update(Person element) {
         //todo написать реализацию
-        return null;
+        persons.put(element.getId(), element);
+        return element;
     }
 
     public void delete(Person element) {
         //todo написать реализацию
+        persons.remove(element.getId());
     }
 }
